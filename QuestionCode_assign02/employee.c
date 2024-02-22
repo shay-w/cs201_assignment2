95d60d95545faab572813000da39d60c1b84f063
shay-w <124108929+shay-w@users.noreply.github.com>
2024-02-21 19:50:14 -0500

    Add files via upload

---
+++ QuestionCode_assign02/employee.c
@@ -0,0 +1,22 @@
+#include "employee.h"
+EMPLOYEE* list_of_employees=NULL;
+
+void printEmployee(EMPLOYEE e){
+    // Your code here
+}
+
+void addEmployee(int e_id, char* e_name){
+    // Your code here
+}
+
+void printEmployees(){
+    // Your code here
+}
+
+int addPayment(int e_id, char* p_date, float p_amount){
+    // Your code here
+}
+
+int deleteEmployee(int e_id, char* e_name){
+    // Your code here
+}
